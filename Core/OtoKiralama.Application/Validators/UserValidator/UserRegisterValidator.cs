06e960cb58d50f1ab417a6135b4aed38f980b053
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-07 14:41:25 -0700

    UserRegisterDto && UserRegisterValidator is Done (Kullanıcı giriş sistemi temelleri yapıldı.)

---
+++ Core/OtoKiralama.Application/Validators/UserValidator/UserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.User;
+
+namespace OtoKiralama.Application.Validators.UserValidator
+{
+    public class UserRegisterValidator : AbstractValidator<RegisterDto>
+    {
+        public UserRegisterValidator()
+        {
+            RuleFor(r => r.FullName)
+                .NotEmpty()
+                .MaximumLength(30);
+            RuleFor(r => r.UserName)
+                .NotEmpty()
+                .MaximumLength(30);
+            RuleFor(r => r.Email)
+                .NotEmpty()
+                .EmailAddress();
+            RuleFor(r => r.Password)
+                .NotEmpty()
+                .MaximumLength(15)
+                .MinimumLength(6);
+            RuleFor(r => r.RePassword)
+                .Equal(r => r.Password);
+        }
+    }
+}
---
+++ Presentation/OtoKiralama.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OtoKiralama.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
