31daec26377d5529c59df6a639953fa7fdf051fc
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-07 15:41:51 -0700

    User Registeration Logic is Done (Kullanıcı kayıt mantığı bitti)

--- Presentation/OtoKiralama.Presentation/Controllers/AuthController.cs
+++ Presentation/OtoKiralama.Presentation/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OtoKiralama.Application.Dtos.User;
+using OtoKiralama.Persistance.Entities;
 
 namespace OtoKiralama.Presentation.Controllers
 {
@@ -7,10 +10,42 @@ namespace OtoKiralama.Presentation.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Register()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return Ok();
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var existUser = await _userManager.FindByNameAsync(registerDto.UserName);
+            if (existUser is not null)
+                return BadRequest();
+            AppUser user = new()
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                FullName = registerDto.FullName,
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+            await _userManager.AddToRoleAsync(user, "member");
+
+            return StatusCode(201);
+        }
+        //[HttpGet]
+        //public async Task<IActionResult> CreateRole()
+        //{
+        //    if (!await _roleManager.RoleExistsAsync("admin"))
+        //        await _roleManager.CreateAsync(new IdentityRole() { Name = "admin" });
+        //    if (!await _roleManager.RoleExistsAsync("member"))
+        //        await _roleManager.CreateAsync(new IdentityRole() { Name = "member"});
+        //    return StatusCode(201);
+        //}
     }
 }
