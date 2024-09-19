37ed28da876e5aaabbdbf489f40419cc018ed610
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-19 12:15:51 -0700

    Brand Service is Written with UnitOfWork

--- Core/OtoKiralama.Application/Services/BrandService.cs
+++ Core/OtoKiralama.Application/Services/BrandService.cs
@@ -6,47 +6,49 @@ using OtoKiralama.Application.Exceptions;
 using OtoKiralama.Application.Interfaces;
 using OtoKiralama.Domain.Entities;
 using OtoKiralama.Persistance.Data;
+using OtoKiralama.Persistance.Data.Implementations;
 
 namespace OtoKiralama.Application.Services
 {
     public class BrandService : IBrandService
     {
-        private readonly AppDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public BrandService(AppDbContext context, IMapper mapper)
+        public BrandService(AppDbContext context, IMapper mapper, IUnitOfWork unitOfWork)
         {
-            _context = context;
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task CreateBrandAsync(BrandCreateDto brandCreateDto)
         {
             var brand = _mapper.Map<Brand>(brandCreateDto);
-            var existBrand = await _context.Brands.AnyAsync(c => c.Name == brand.Name);
+            var existBrand = await _unitOfWork.BrandRepository.isExists(b => b.Name == brand.Name);
             if (existBrand)
                 throw new CustomException(400, "Name", "Brand already exist with this name");
-            await _context.Brands.AddAsync(brand);
-            await _context.SaveChangesAsync();
+            await _unitOfWork.BrandRepository.Create(brand);
+            _unitOfWork.Commit();
         }
 
         public async Task DeleteBrandAsync(int id)
         {
-            var brand = await _context.Brands.FindAsync(id);
+            var brand = await _unitOfWork.BrandRepository.GetEntity(b => b.Id == id);
             if (brand is null)
                 throw new CustomException(404, "Id", "Brand not found with this Id");
-            _context.Brands.Remove(brand);
-            await _context.SaveChangesAsync();
+            await _unitOfWork.BrandRepository.Delete(brand);
+            _unitOfWork.Commit();
         }
 
         public async Task<List<BrandReturnDto>> GetAllBrandsAsync()
         {
-            return await _context.Brands.Select(b => _mapper.Map<BrandReturnDto>(b)).ToListAsync();
+            var brands = await _unitOfWork.BrandRepository.GetAll();
+            return _mapper.Map<List<BrandReturnDto>>(brands);
         }
 
         public async Task<BrandReturnDto> GetBrandByIdAsync(int id)
         {
-            var brand = await _context.Brands.FindAsync(id);
+            var brand = await _unitOfWork.BrandRepository.GetEntity(b => b.Id == id);
             if (brand is null)
                 throw new CustomException(404, "Id", "Brand not found with this Id");
             return _mapper.Map<BrandReturnDto>(brand);
