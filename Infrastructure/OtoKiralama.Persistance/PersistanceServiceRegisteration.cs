55ab058dc9c019a051c052fd45bfd1d66bb2ffa6
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-19 11:49:46 -0700

    Persistance Service Registeration Extension to IServiceCollection is Created

---
+++ Infrastructure/OtoKiralama.Persistance/PersistanceServiceRegisteration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OtoKiralama.Domain.Repositories;
+using OtoKiralama.Persistance.Data.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoKiralama.Persistance
+{
+    public static class PersistanceServiceRegisteration
+    {
+        public static void Register(this IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IBrandRepository, BrandRepository>();
+        }
+    }
+}
