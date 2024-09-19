f977d97cc37aaed91cdf7bfb9e2bd94bc278a19e
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-19 13:55:00 -0700

    Role Service is Created

---
+++ Core/OtoKiralama.Application/Services/RoleService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OtoKiralama.Application.Dtos.Role;
+using OtoKiralama.Application.Interfaces;
+
+namespace OtoKiralama.Application.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RoleService(RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<RoleReturnDto>> GetAllRolesAsync()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return _mapper.Map<List<RoleReturnDto>>(roles);
+        }
+    }
+}
