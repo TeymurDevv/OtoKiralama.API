acb267b2ea4a856d846efb68ee5718c6dd98f78c
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-22 10:22:00 -0700

    Role Create Validator Is Done

---
+++ Core/OtoKiralama.Application/Validators/RoleValidator/RoleCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.Role;
+
+namespace OtoKiralama.Application.Validators.RoleValidator
+{
+    public class RoleCreateValidator : AbstractValidator<RoleCreateDto>
+    {
+        public RoleCreateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        }
+    }
+}
