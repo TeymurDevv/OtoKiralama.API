5e26e2043436b97db763137080b68020a81669bc
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 10:02:52 -0700

    Gear Repository Class Is Created

---
+++ Infrastructure/OtoKiralama.Persistance/Data/Implementations/GearRepository.cs
@@ -0,0 +1,12 @@
+﻿using OtoKiralama.Domain.Entities;
+using OtoKiralama.Domain.Repositories;
+
+namespace OtoKiralama.Persistance.Data.Implementations
+{
+    public class GearRepository : Repository<Gear>, IGearRepository
+    {
+        public GearRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
