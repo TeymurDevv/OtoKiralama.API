30604fb2983385c0940272d8f234519cd2dc9bbd
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 09:48:26 -0700

    Gear Serice Interface Is Created

---
+++ Core/OtoKiralama.Application/Interfaces/IGearService.cs
@@ -0,0 +1,10 @@
+﻿namespace OtoKiralama.Application.Interfaces
+{
+    public interface IGearService
+    {
+        Task<List<GearReturnDto>> GetAllGearsAsync();
+        Task<GearReturnDto> GetGearByIdAsync(int id);
+        Task CreateGearAsync(GearCreateDto gearCreateDto);
+        Task DeleteGearAsync(int id);
+    }
+}
