a28f1818ca0dc348aafdcf06b5c5467cefed118b
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 10:12:27 -0700

    Gear Model is Added to database Like Dbset<> property

--- Infrastructure/OtoKiralama.Persistance/Data/AppDbContext.cs
+++ Infrastructure/OtoKiralama.Persistance/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace OtoKiralama.Persistance.Data
 {
     public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public DbSet<Gear> Gears { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public AppDbContext(DbContextOptions options) : base(options)
