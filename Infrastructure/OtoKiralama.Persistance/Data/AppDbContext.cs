5b67fb6aa9d0190f45984223e2218ffe1a565171
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 10:12:49 -0700

    Using Sort

--- Infrastructure/OtoKiralama.Persistance/Data/AppDbContext.cs
+++ Infrastructure/OtoKiralama.Persistance/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OtoKiralama.Domain.Entities;
 using OtoKiralama.Persistance.Entities;
