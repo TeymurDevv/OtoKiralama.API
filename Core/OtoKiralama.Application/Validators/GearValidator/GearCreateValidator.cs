b23342cd381ca68c435d15f708722d5706f9a2ec
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 10:31:38 -0700

    GearCreateValidator Is Created

---
+++ Core/OtoKiralama.Application/Validators/GearValidator/GearCreateValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.Gear;
+
+namespace OtoKiralama.Application.Validators.GearValidator
+{
+    public class GearCreateValidator : AbstractValidator<GearCreateDto>
+    {
+        public GearCreateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Name).MaximumLength(15).WithMessage("Name can't be more than 50 character");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Name can't be less than 3 character");
+        }
+    }
+}
