c7b579afb621b27da2c067cb27781f0dd9e4cf64
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 10:04:29 -0700

    Gear Repository Added to the IUnitOfWork Interface

--- Infrastructure/OtoKiralama.Persistance/Data/Implementations/IUnitOfWork.cs
+++ Infrastructure/OtoKiralama.Persistance/Data/Implementations/IUnitOfWork.cs
@@ -6,6 +6,7 @@ namespace OtoKiralama.Persistance.Data.Implementations
     {
         public ILocationRepository LocationRepository { get; }
         public IBrandRepository BrandRepository { get; }
+        public IGearRepository GearRepository { get; }
         public void Commit();
 
     }
