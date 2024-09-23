27e31f078555fd1e26d013e31f07b261275ae1f9
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-23 09:54:51 -0700

    Gear Service Dependency Injection

---
+++ Core/OtoKiralama.Application/Services/GearService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using OtoKiralama.Application.Dtos.Gear;
+using OtoKiralama.Application.Interfaces;
+using OtoKiralama.Persistance.Data.Implementations;
+
+namespace OtoKiralama.Application.Services
+{
+    public class GearService : IGearService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GearService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task CreateGearAsync(GearCreateDto gearCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteGearAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<GearReturnDto>> GetAllGearsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GearReturnDto> GetGearByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
