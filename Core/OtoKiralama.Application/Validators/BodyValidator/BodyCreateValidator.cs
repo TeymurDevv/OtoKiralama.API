a03e24064815ad86c63af109b74359a0be51c058
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 08:18:07 -0700

    BodyCreateValidator has been Created

---
+++ Core/OtoKiralama.Application/Validators/BodyValidator/BodyCreateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.Body;
+
+namespace OtoKiralama.Application.Validators.BodyValidator
+{
+    public class BodyCreateValidator: AbstractValidator<BodyCreateDto>
+    {
+        public BodyCreateValidator()
+        {
+            RuleFor(c => c.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MinimumLength(3)
+            .WithMessage("Name must be at least 3 characters long");
+        }
+    }
+}
