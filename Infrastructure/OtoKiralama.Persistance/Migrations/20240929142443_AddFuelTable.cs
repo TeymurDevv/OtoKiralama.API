e8cfd8a9c491fbbcc258bca8c9d7ff473e1b7d28
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 07:24:56 -0700

    Fuel Migration Created

---
+++ Infrastructure/OtoKiralama.Persistance/Migrations/20240929142443_AddFuelTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoKiralama.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFuelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fuels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fuels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fuels");
+        }
+    }
+}
