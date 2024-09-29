a3122d80e3a1a5354db84cafe6d9729d50572b25
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 14:06:04 -0700

    FuelService Dependency injection and Interface ImplementationIs Done

---
+++ Core/OtoKiralama.Application/Services/FuelService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using OtoKiralama.Application.Dtos.Fuel;
+using OtoKiralama.Application.Interfaces;
+using OtoKiralama.Persistance.Data.Implementations;
+
+namespace OtoKiralama.Application.Services
+{
+    public class FuelService : IFuelService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public FuelService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task CreateFuelAsync(FuelCreateDto fuelCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteFuelAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FuelReturnDto>> GetAllFuelsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FuelReturnDto> GetFuelByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
