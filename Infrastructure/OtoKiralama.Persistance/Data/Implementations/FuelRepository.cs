82c3e00ea1cee17c294cc2d7a01d558c0ba6b17b
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 03:50:21 -0700

    FuelRepository has been Created

---
+++ Infrastructure/OtoKiralama.Persistance/Data/Implementations/FuelRepository.cs
@@ -0,0 +1,12 @@
+﻿using OtoKiralama.Domain.Entities;
+using OtoKiralama.Domain.Repositories;
+
+namespace OtoKiralama.Persistance.Data.Implementations
+{
+    public class FuelRepository:Repository<Fuel>,IFuelRepository
+    {
+        public FuelRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
