f8ac24bbf75b9289a1920f9b04f36661febd6fa8
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 14:00:24 -0700

    IFuelService Methods Rename

--- Core/OtoKiralama.Application/Interfaces/IFuelService.cs
+++ Core/OtoKiralama.Application/Interfaces/IFuelService.cs
@@ -4,9 +4,9 @@ namespace OtoKiralama.Application.Interfaces
 {
     public interface IFuelService
     {
-        Task<List<GearReturnDto>> GetAllGearsAsync();
-        Task<GearReturnDto> GetGearByIdAsync(int id);
-        Task CreateGearAsync(GearCreateDto gearCreateDto);
-        Task DeleteGearAsync(int id);
+        Task<List<GearReturnDto>> GetAllFuelsAsync();
+        Task<GearReturnDto> GetFuelByIdAsync(int id);
+        Task CreateFuelAsync(GearCreateDto gearCreateDto);
+        Task DeleteFuelAsync(int id);
     }
 }
