51cf795a3cc802dbf61506055cf26dca7f55969b
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 14:01:46 -0700

    Fuel Create & Return Dto have been Created

---
+++ Core/OtoKiralama.Application/Dtos/Fuel/FuelCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace OtoKiralama.Application.Dtos.Fuel
+{
+    public class FuelCreateDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/OtoKiralama.Application/Dtos/Fuel/FuelReturnDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OtoKiralama.Application.Dtos.Fuel
+{
+    public class FuelReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
