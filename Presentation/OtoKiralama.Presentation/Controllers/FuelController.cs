c2ec5a52eaea3673f99cbca2e35bfb2ed1cc1642
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 14:32:06 -0700

    Fuel Controller is Done

---
+++ Presentation/OtoKiralama.Presentation/Controllers/FuelController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OtoKiralama.Application.Dtos.Fuel;
+using OtoKiralama.Application.Interfaces;
+
+namespace OtoKiralama.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuelController : ControllerBase
+    {
+        private readonly IFuelService _fuelService;
+
+        public FuelController(IFuelService fuelService)
+        {
+            _fuelService = fuelService;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllFuels()
+        {
+            return Ok(await _fuelService.GetAllFuelsAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFuelById(int id)
+        {
+            return Ok(await _fuelService.GetFuelByIdAsync(id));
+        }
+        [HttpPost("")]
+        public async Task<IActionResult> CreateFuel(FuelCreateDto fuelCreateDto)
+        {
+            await _fuelService.CreateFuelAsync(fuelCreateDto);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFuelById(int id)
+        {
+            await _fuelService.DeleteFuelAsync(id);
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+    }
+}
