87d7b983f266e2a727a6f2d67d9dcd6653b262b1
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-29 14:10:51 -0700

    Fuel Service is Done

--- Core/OtoKiralama.Application/Services/FuelService.cs
+++ Core/OtoKiralama.Application/Services/FuelService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using OtoKiralama.Application.Dtos.Body;
 using OtoKiralama.Application.Dtos.Fuel;
+using OtoKiralama.Application.Exceptions;
 using OtoKiralama.Application.Interfaces;
+using OtoKiralama.Domain.Entities;
 using OtoKiralama.Persistance.Data.Implementations;
 
 namespace OtoKiralama.Application.Services
@@ -16,24 +19,37 @@ namespace OtoKiralama.Application.Services
             _mapper = mapper;
         }
 
-        public Task CreateFuelAsync(FuelCreateDto fuelCreateDto)
+        public async Task CreateFuelAsync(FuelCreateDto fuelCreateDto)
         {
-            throw new NotImplementedException();
+            var fuel = _mapper.Map<Fuel>(fuelCreateDto);
+            var existFuel = await _unitOfWork.FuelRepository.isExists(f => f.Name == fuelCreateDto.Name);
+            if (existFuel)
+                throw new CustomException(400, "Name", "Fuel already exist with this name");
+            await _unitOfWork.FuelRepository.Create(fuel);
+            _unitOfWork.Commit();
         }
 
-        public Task DeleteFuelAsync(int id)
+        public async Task DeleteFuelAsync(int id)
         {
-            throw new NotImplementedException();
+            var fuel = await _unitOfWork.FuelRepository.GetEntity(f => f.Id == id);
+            if (fuel is null)
+                throw new CustomException(404, "Id", "Fuel not found with this Id");
+            await _unitOfWork.FuelRepository.Delete(fuel);
+            _unitOfWork.Commit();
         }
 
-        public Task<List<FuelReturnDto>> GetAllFuelsAsync()
+        public async Task<List<FuelReturnDto>> GetAllFuelsAsync()
         {
-            throw new NotImplementedException();
+            var fuels = await _unitOfWork.FuelRepository.GetAll();
+            return _mapper.Map<List<FuelReturnDto>>(fuels);
         }
 
-        public Task<FuelReturnDto> GetFuelByIdAsync(int id)
+        public async Task<FuelReturnDto> GetFuelByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var fuel = await _unitOfWork.FuelRepository.GetEntity(f => f.Id == id);
+            if (fuel is null)
+                throw new CustomException(404, "Id", "Fuel not found with this Id");
+            return _mapper.Map<FuelReturnDto>(fuel);
         }
     }
 }
