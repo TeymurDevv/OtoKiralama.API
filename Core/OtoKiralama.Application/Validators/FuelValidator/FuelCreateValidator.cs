ad4d59da41c2a9b4d6c80eb259b723ceb149dc9d
TeymurDevv <transmissiyaservis@gmail.com>
2024-10-02 02:57:03 -0700

    FuelCreateValidator has been Created

---
+++ Core/OtoKiralama.Application/Validators/FuelValidator/FuelCreateValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.Fuel;
+
+namespace OtoKiralama.Application.Validators.FuelValidator
+{
+    public class FuelCreateValidator : AbstractValidator<FuelCreateDto>
+    {
+        public FuelCreateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x=> x.Name).MaximumLength(50).WithMessage("Name can't be more than 50 character");
+            RuleFor(x=>x.Name).MinimumLength(3).WithMessage("Name can't be more than 3 character");
+        }
+    }
+}
