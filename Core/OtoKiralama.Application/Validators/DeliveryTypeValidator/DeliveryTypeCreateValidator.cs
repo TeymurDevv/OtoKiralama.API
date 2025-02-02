ad1a6e1074ece6303d93b68e532d9f07ce7d5138
code810 <nadirssh@code.edu.az>
2025-02-02 23:29:18 +0400

    CarPhotDtos Profile DeliveryType Validator Delivery Type ready

---
+++ Core/OtoKiralama.Application/Validators/DeliveryTypeValidator/DeliveryTypeCreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OtoKiralama.Application.Dtos.DeliveryType;
+
+namespace OtoKiralama.Application.Validators.DeliveryTypeValidator
+{
+    public class DeliveryTypeCreateValidator : AbstractValidator<DeliveryTypeCreateDto>
+    {
+        public DeliveryTypeCreateValidator()
+        {
+            RuleFor(c => c.Name)
+            .NotEmpty()
+            .WithMessage("İsim alanı boş olamaz.")
+            .MinimumLength(2)
+            .WithMessage("isim metni en az 2 karakter uzunluğunda olmak zorunda.")
+             .MaximumLength(50)
+            .WithMessage("isim metni en fazla 50 karakter uzunluğunda ola bilir.");
+        }
+    }
+}
+
--- Infrastructure/OtoKiralama.Persistance/Migrations/20250202190509_AddDeliveryTypeToTable.Designer.cs
+++ Infrastructure/OtoKiralama.Persistance/Migrations/20250202190509_AddDeliveryTypeToTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OtoKiralama.Persistance.Data;
 
@@ -11,9 +12,11 @@ using OtoKiralama.Persistance.Data;
 namespace OtoKiralama.Persistance.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250202190509_AddDeliveryTypeToTable")]
+    partial class AddDeliveryTypeToTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
