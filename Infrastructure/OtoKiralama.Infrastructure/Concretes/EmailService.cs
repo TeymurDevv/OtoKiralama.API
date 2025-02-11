67b592dbc7e73b9a4908d863bcbeb8ceb03b6485
TeymurDevv <transmissiyaservis@gmail.com>
2025-02-11 14:27:58 +0400

    ResendService NugetPackage Is Installed And Dependency has been Injected

--- Infrastructure/OtoKiralama.Infrastructure/Concretes/EmailService.cs
+++ Infrastructure/OtoKiralama.Infrastructure/Concretes/EmailService.cs
@@ -1,9 +1,15 @@
 using OtoKiralama.Application.Interfaces;
+using Resend;
 
 namespace OtoKiralama.Infrastructure.Concretes;
 
 public class EmailService : IEmailService
 {
+    private readonly IResend _resend;
+    public EmailService(IResend resend)
+    {
+        _resend = resend;
+    }
     public Task SendEmailAsync(string to, string subject, string body, bool isHtml = true)
     {
         throw new NotImplementedException();
