843b9bb18625710be691a4646e32b1747b6da2d8
TeymurDevv <transmissiyaservis@gmail.com>
2025-02-11 22:00:54 +0400

    IReservationService Interface has been Updated

--- Core/OtoKiralama.Domain/Repositories/IReservationRepository.cs
+++ Core/OtoKiralama.Domain/Repositories/IReservationRepository.cs
@@ -5,6 +5,7 @@ namespace OtoKiralama.Domain.Repositories
     public interface IReservationRepository : IRepository<Reservation>
     {
         Task<int> CountAsync();
+        Task<int> CountAsync(Predicate<Reservation> predicate);
         Task<int> GetLastReservationNumberForYear(int year);
     }
 }
