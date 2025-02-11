867c89b549341e631c8be52ec88c04cdfd8aa9cf
TeymurDevv <transmissiyaservis@gmail.com>
2025-02-11 23:53:32 +0400

    RegisterAsync Method Is Done

--- Core/OtoKiralama.Application/Services/AuthService.cs
+++ Core/OtoKiralama.Application/Services/AuthService.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using OtoKiralama.Application.Dtos.User;
+using OtoKiralama.Application.Exceptions;
 using OtoKiralama.Application.Interfaces;
 using OtoKiralama.Application.Settings;
 using OtoKiralama.Domain.Entities;
@@ -26,9 +27,25 @@ public class AuthService : IAuthService
         _jwtSettings = jwtSettings.Value;
         _unitOfWork = unitOfWork;
     }
-    public Task RegisterAsync(RegisterDto registerDto)
+    public async Task RegisterAsync(RegisterDto registerDto)
     {
-        throw new NotImplementedException();
+        if (_userManager.Users.Any(u => u.Email == registerDto.Email))
+            throw new CustomException(400, "Email", "Email already in use");
+        var existUser = await _userManager.FindByNameAsync(registerDto.UserName);
+        if (existUser != null) 
+            throw new CustomException(400, "Username", "Username already in use");
+        AppUser appUser = new AppUser();
+        appUser.UserName = registerDto.UserName;
+        appUser.Email = registerDto.Email;
+        appUser.FullName = registerDto.FullName;
+        var result = await _userManager.CreateAsync(appUser, registerDto.Password);
+        if (!result.Succeeded)
+        {
+            var errorMessages = result.Errors.ToDictionary(e => e.Code, e => e.Description);
+
+            throw new CustomException(400, errorMessages);
+        }
+        await _userManager.AddToRoleAsync(appUser, "member");
     }
 
     public Task CompanyPersonelRegisterAsync(RegisterCompanyUserDto registerCompanyUserDto)
