e7c603d87eb1a6de3c869f7b62e18e412b40d471
TeymurDevv <transmissiyaservis@gmail.com>
2025-02-11 23:49:46 +0400

    Dependencies have been Injected

--- Core/OtoKiralama.Application/Services/AuthService.cs
+++ Core/OtoKiralama.Application/Services/AuthService.cs
@@ -1,10 +1,31 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using OtoKiralama.Application.Dtos.User;
 using OtoKiralama.Application.Interfaces;
+using OtoKiralama.Application.Settings;
+using OtoKiralama.Domain.Entities;
+using OtoKiralama.Domain.Repositories;
 
 namespace OtoKiralama.Application.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly UserManager<AppUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ITokenService _tokenService;
+    private readonly JwtSettings _jwtSettings;
+    private readonly IUnitOfWork _unitOfWork;
+    public AuthService(IOptions<JwtSettings> jwtSettings, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IMapper mapper, ITokenService tokenService, IUnitOfWork unitOfWork, IHttpContextAccessor contextAccessor)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _tokenService = tokenService;
+        _jwtSettings = jwtSettings.Value;
+        _unitOfWork = unitOfWork;
+    }
     public Task RegisterAsync(RegisterDto registerDto)
     {
         throw new NotImplementedException();
