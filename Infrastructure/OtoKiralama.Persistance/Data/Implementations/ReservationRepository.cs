7576d1aa53042368798c166a35952dfcb338c8cd
TeymurDevv <transmissiyaservis@gmail.com>
2025-02-11 22:04:22 +0400

    Predicate added

--- Infrastructure/OtoKiralama.Persistance/Data/Implementations/ReservationRepository.cs
+++ Infrastructure/OtoKiralama.Persistance/Data/Implementations/ReservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿    using System.Linq.Expressions;
+    using Microsoft.EntityFrameworkCore;
 using OtoKiralama.Domain.Entities;
 using OtoKiralama.Domain.Repositories;
 
@@ -15,6 +16,11 @@ namespace OtoKiralama.Persistance.Data.Implementations
         public async Task<int> CountAsync()
         {
             return await _context.Set<Reservation>().CountAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<Reservation, bool>> predicate)
+        {
+            return await _context.Set<Reservation>().CountAsync(predicate);
         }
 
         public async Task<int> GetLastReservationNumberForYear(int year)
